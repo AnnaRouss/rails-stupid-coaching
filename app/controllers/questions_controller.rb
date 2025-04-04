29953f8e64865141e9fb1dbbc742939b2e403676
AnastasiaRoussakis <a_roussakis@outlook.com>
2025-04-04 08:35:24 -0400

    Updated Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,13 +4,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-
     if @question.blank?
       @answer = "I can't hear you!"
-    elsif @question.downcase == "i am going to work"
-      @answer = "Great!"
-    elsif @question.end_with?("?")
-      @answer = "Silly question, get dressed and go to work!"
+    elsif @question =~ /i am going to work/i
+      @answer = 'Great!'
+    elsif @question.ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
